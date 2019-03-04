5cc4344df2ee54bdfd1d07386dacf453bb0d3acf
Ernesto Solares <ernesto.solares.guerrero@gmail.com>
2019-03-03 20:51:35 -0600

    finished word game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,135 @@
 
-        // number of wins, lose, tie
-        var win = 0;
-        var lose = 0;
-        var tie = 0;
-        var possibleGuesses = "abcdefghijklmnopqrstuvwxyz";
-        var guessesLeft = 9;
-        var currentguessesLeft = guessesLeft;
-        var shouldCalculateNewLetter = true;
+// Number of wins, lose
+var win = 0;
+var lose = 0;
+
+// Possible words array
+var possibleWords = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou",
+    "beekeeper", "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing",
+    "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves",
+    "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove",
+    "frazzled", "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo", "giaour", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip",
+    "grogginess", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest",
+    "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak", "kazoo", "keyhole",
+    "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph", "marquis", "matrix", "megahertz",
+    "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo",
+    "phlegm", "pixel", "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz", "queue", "quips", "quixotic", "quiz", "quizzes",
+    "quorum", "razzmatazz", "rhubarb", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength",
+    "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress",
+    "transplant", "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex",
+    "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard",
+    "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch",
+    "zipper", "zodiac", "zombie", "View all"]
+
+var guessesLeft = 9;
+var currentguessesLeft = guessesLeft;
+var shouldCalculateNewWord = true;
+var currentGuesses = "";
+var userGuess;
+var wordToGuess;
+var currentGuessed = "";
+
+// we get the html tags in some variables
+var winText = document.getElementById("win-text");
+var loseText = document.getElementById("lose-text");
+var wordToGuessText = document.getElementById("word-to-guess");
+var guessesLeftText = document.getElementById("remaining-guesses");
+var guessedLettersText = document.getElementById("guessed-letters");
+
+// HACK FOR TESTING
+// var computerText = document.getElementById("hack")
+
+//we set the current guesses left text to its default value
+guessesLeftText.textContent = currentguessesLeft;
+
+GenerateNewWord();
+
+function GenerateNewWord() {
+    if (shouldCalculateNewWord) {
+        wordToGuess = possibleWords[Math.floor(Math.random() * possibleWords.length)].toString();
+        // computerText.textContent = wordToGuess; //HACK FOR TESTING
+        DashGenerator(wordToGuess);
+        shouldCalculateNewWord = false;
+        console.log("Clever to see the console, here is the answer: " + wordToGuess);
+    }
+}
 
 
-        // we get the html tags in some variables
-        var winText = document.getElementById("winsText");
-        var loseText = document.getElementById("loosesText");
+function Reset() {
 
-        var computerText = document.getElementById("computer")
-        var userText = document.getElementById("GuessesText")
-        var guessesLeftText = document.getElementById("guessesLeftText");
-        guessesLeftText.textContent = currentguessesLeft;
+    currentGuessed = "";
 
-        function Reset() {
-            currentguessesLeft = guessesLeft;
-            currentGuesses = "";
-            GuessesText.textContent = "";
-            shouldCalculateNewLetter = true;
-            guessesLeftText.textContent = currentguessesLeft;
+    // Reset current guesses left and text
+    currentguessesLeft = guessesLeft;
+    guessesLeftText.textContent = currentguessesLeft;
+
+    // Reset already guessed letters and text
+    currentGuesses = "";
+    guessedLettersText.textContent = "_";
+
+    // Change booleant to generate a new word in the next keypress
+    shouldCalculateNewWord = true;
+    GenerateNewWord();
+}
+
+function DashGenerator(word) {
+    wordToGuessText.textContent = "";
+    for (var i = 0; i < word.length; i++) {
+        wordToGuessText.textContent += "_";
+        currentGuessed += "_";
+    }
+}
+
+function UpdateDashWord(index, letter) {
+    currentGuessed = currentGuessed.substring(0,index) + letter + currentGuessed.substring(index+1);
+    wordToGuessText.textContent = currentGuessed;
+}
+
+function CheckGameStatus() {
+    if (currentGuessed == wordToGuess) {
+        win++;
+        UpdateScore();
+        Reset();
+    }
+
+    if(currentguessesLeft == 0){
+        lose++;
+        UpdateScore();
+        Reset();
+    }
+}
+
+function UpdateScore(){
+    winText.textContent = win;
+    loseText.textContent = lose;
+}
+
+function CheckLetter(letter) {
+    var count = 0;
+    for (var i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess.charAt(i) == letter) {
+            UpdateDashWord(i, letter);
+            count++;
         }
-        var currentGuesses = "";
-        var computerGuess;
-        var userGuess;
-        // This function is run whenever the user presses a key.
-        document.onkeyup = function (event) {
+    }
 
-            // Determines which key was pressed.
-            userGuess = event.key;
-            if (shouldCalculateNewLetter) {
-                computerGuess = possibleGuesses.charAt(Math.floor(Math.random() * possibleGuesses.length));
-                computerText.textContent = computerGuess;
-                shouldCalculateNewLetter = false;
-            }
+    if(count == 0){
+        currentguessesLeft--;
+        guessesLeftText.textContent = currentguessesLeft;
+    }
+}
 
-            userText.textContent = userGuess;
-            currentGuesses += userGuess + ", " ;
-            if (userGuess == computerGuess) {
-                win++;
-                Reset();
-            }
-            else {
-                currentguessesLeft--;
-                userText.textContent = currentGuesses;
-                if (currentguessesLeft == 0) {
-                    lose++;
-                    Reset();
-                }
-            }
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
 
-            winText.textContent = win;
-            loseText.textContent = lose;
-            guessesLeftText.textContent = currentguessesLeft;
+    // Calculate a new word if needed
+    GenerateNewWord();
 
-            // Randomly chooses a choice from the options array. This is the Computer's guess.
-        };
+    // Determines which key was pressed and add it to the current guessed letters
+    userGuess = event.key;
+    currentGuesses += userGuess + ", ";
+    guessedLettersText.textContent = currentGuesses;
+
+    CheckLetter(userGuess);
+    CheckGameStatus(); 
+};
